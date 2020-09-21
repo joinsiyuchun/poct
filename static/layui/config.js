500e75af1b6218c4663f33a40a92bbb1024e43dc
joinsiyuchun <348977654@qq.com>
2020-09-21 23:35:37 +0800

    modified:   application/index/view/index/home.html

--- static/layui/config.js
+++ static/layui/config.js
@@ -1,11 +1,4 @@
-/**
 
- @Name：layuiAdmin iframe版全局配置
- @Author：贤心
- @Site：http://www.layui.com/admin/
- @License：LPPL（layui付费产品协议）
-    
- */
  
 layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
   exports('setter', {
