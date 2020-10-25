6aa014274b72bc88e09fe86e6d1a395b451b867b
zhaomingxing <zhaomingxing@medatc.com>
2020-10-25 20:44:16 +0800

    add did logic

--- config/database.php
+++ config/database.php
@@ -13,13 +13,14 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => 'bdm719189558.my3w.com',
     // 数据库名
     'database'        => 'bdm719189558_db',
     // 用户名
-    'username'        => 'root',
+    // 用户名
+    'username'        => 'bdm719189558',
     // 密码
-    'password'        => '12345678',
+    'password'        => 'Join379610',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
