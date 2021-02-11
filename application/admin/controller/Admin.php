437fbfcffcd224d3774b1d5b26b1f4ab4b0e5c0c
joinsiyuchun <348977654@qq.com>
2021-02-11 23:49:53 +0800

    modified:   application/admin/controller/Admin.php 	modified:   application/admin/view/admin/add.html 	modified:   application/admin/view/admin/edit.html 	modified:   application/admin/view/admin/index.html 	config/database_bk.php

--- application/admin/controller/Admin.php
+++ application/admin/controller/Admin.php
@@ -6,20 +6,21 @@ namespace app\admin\controller;
 
 use app\admin\common\controller\Base;
 use app\admin\common\model\Role;
+use app\admin\common\model\Group;
 use think\db;
 use think\facade\Request;
 use app\admin\common\model\Admin as AdminModel;
 
 class Admin extends Base
 {
-    // 管理员管理首页
+    // 用户管理首页
     public function index()
     {
-        $this -> view -> assign('title', '管理员管理');
+        $this -> view -> assign('title', '用户管理');
         return $this -> view -> fetch('index');
     }
 
-    // 管理员列表
+    // 用户列表
     public function adminList()
     {
         // 定义全局查询条件
@@ -35,12 +36,13 @@ class Admin extends Base
         $limit = isset($_GET['limit']) ? $_GET['limit'] : 10;
         $page = isset($_GET['page']) ? $_GET['page'] : 1;
 
-        // 获取到所有的管理员
+        // 获取到所有的用户
         $adminList = AdminModel::where($map)
             -> alias('a')
             -> join(['think_role' => 'r'], 'a.role_id = r.id')
+            -> join(['think_group' => 'f'], 'a.group_id = f.id')
             -> order('a.id', 'desc')
-            -> field('a.id, a.username, a.role_id, a.status, a.create_time, a.update_time, a.last_login_time, r.name')
+            -> field('a.id, a.username, a.role_id, a.group_id,a.status, a.create_time, a.update_time, a.last_login_time, r.name,f.name gname')
             -> select();
 
         $total = count(AdminModel::where($map)->select());
@@ -54,21 +56,24 @@ class Admin extends Base
         return $this -> view -> fetch('index');
     }
 
-    // 添加管理员
+    // 添加用户
     public function add()
     {
-        // 获取所有非超级管理员角色
+        // 获取所有非超级用户角色
         $roleList = Role::where('id', '>', 1) -> select();
+        // 获取所有维修组
+        $groupList = Group::where('status', 1) -> select();
 
         // 设置模板变量
-        $this -> view -> assign('title', '添加管理员');
+        $this -> view -> assign('title', '添加用户');
         $this -> view -> assign('roleList', $roleList);
+        $this -> view -> assign('groupList', $groupList);
 
         // 渲染模板
         return $this -> view -> fetch('add');
     }
 
-    // 执行管理员添加操作
+    // 执行用户添加操作
     public function doAdd()
     {
         // 获取用户提交的信息
@@ -78,38 +83,41 @@ class Admin extends Base
         // 执行添加操作
         try {
             $node = AdminModel::where('username', $data['username']) -> find();
-            if ( !empty($role)) {
+            if ( !empty($node)) {
                 return resMsg(-1, '用户名已经存在，不能重复添加', 'add');
             }
             AdminModel::create($data);
         } catch (\Exception $e) {
-            return resMsg(0, '管理员添加失败' . '<br>' . $e->getMessage(), 'add' );
+            return resMsg(0, '用户添加失败' . '<br>' . $e->getMessage(), 'add' );
         }
-        return resMsg(1, '管理员添加成功', 'index');
+        return resMsg(1, '用户添加成功', 'index');
     }
 
-    // 编辑管理员
+    // 编辑用户
     public function edit()
     {
         // 获取节点id
         $adminId = Request::param('id');
 
         // 根据节点id查询要更新的节点信息
-        $adminInfo = AdminModel::where('id', $adminId) -> field('id, username, role_id, status') -> find();
+        $adminInfo = AdminModel::where('id', $adminId) -> field('id, username, role_id, group_id,status') -> find();
 
-        // 获取所有非超级管理员角色
+        // 获取所有非超级用户角色
         $roleList = Role::where('id', '>', 1) -> select();
+        // 获取所有维修组
+        $groupList = Group::where('status', 1) -> select();
 
         // 设置模板变量
-        $this -> view -> assign('title', '编辑管理员');
+        $this -> view -> assign('title', '编辑用户');
         $this -> view -> assign('adminInfo', $adminInfo);
         $this -> view -> assign('roleList', $roleList);
+        $this -> view -> assign('groupList', $groupList);
 
         // 渲染模板
         return $this -> view -> fetch('edit');
     }
 
-    // 执行编辑管理员操作
+    // 执行编辑用户操作
     public function doEdit()
     {
         // 1. 获取的用户提交的信息
@@ -131,12 +139,12 @@ class Admin extends Base
             }
             AdminModel::update($data);
         } catch (\Exception $e) {
-            return resMsg(0, '管理员编辑失败' . '<br>' . $e->getMessage(), 'edit' );
+            return resMsg(0, '用户编辑失败' . '<br>' . $e->getMessage(), 'edit' );
         }
-        return resMsg(1, '管理员编辑成功', 'index');
+        return resMsg(1, '用户编辑成功', 'index');
     }
 
-    // 删除管理员
+    // 删除用户
     public function delete()
     {
         if ( Request::isAjax() ) {
@@ -145,9 +153,9 @@ class Admin extends Base
                 $id = Request::param('id');
                 AdminModel::where('id', $id) -> delete();
             } catch (\Exception $e) {
-                return resMsg(0, '管理员删除失败' . '<br>' . $e->getMessage(), 'index' );
+                return resMsg(0, '用户删除失败' . '<br>' . $e->getMessage(), 'index' );
             }
-            return resMsg(1, '管理员删除成功', 'index');
+            return resMsg(1, '用户删除成功', 'index');
         } else {
             return resMsg(-1, '请求类型错误', 'index');
         }
@@ -180,13 +188,13 @@ class Admin extends Base
         return resMsg(1, '<i class="iconfont">&#xe645;</i> 状态变更成功', 'index');
     }
 
-    // 初始化超级管理员密码，生产环境不使用时请注释
+    // 初始化超级用户密码，生产环境不使用时请注释
 //    public function setPwd()
 //    {
 //        // 生成加密的密码
 //        $pwd = makePassword('admin');
 //
-//        // 更新数据库存储的管理员密码
+//        // 更新数据库存储的用户密码
 //        $data = ['password' => $pwd];
 //        $res = AdminModel::where('username', 'admin') -> update($data);
 //        if ( !$res ) {
